3a529f605342033151b517fefe3d6163a217b1c3
m-m-adams <mark.adams@queensu.ca>
2023-12-15 10:54:24 -0500

    custom session reconstruction

---
+++ anomaly_detector/src/ethernet.rs
@@ -0,0 +1,49 @@
+use pcap::Capture;
+use nom::{number, IResult, Finish, bytes};
+use pktparse::{ethernet, ipv4, tcp};
+pub use pktparse::ethernet::{MacAddress, EtherType};
+use sawp::parser::{Direction, Parse};
+use sawp::error::Error;
+use sawp::error::ErrorKind;
+use sawp_modbus::{Modbus, Message};
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct EthernetFrame {
+    pub direction: u16,
+    pub ethtype: u16,
+    pub addr_length: u16,
+    pub source_mac: MacAddress,
+    pub ethertype: EtherType,
+}
+
+pub(crate) fn mac_address(input: &[u8]) -> IResult<&[u8], MacAddress> {
+    let (input, mac) = bytes::streaming::take(6u8)(input)?;
+
+    Ok((input, MacAddress(<[u8; 6]>::try_from(mac).unwrap())))
+}
+
+fn parse_ethertype(input: &[u8]) -> IResult<&[u8], EtherType> {
+    let (input, _) = number::streaming::be_u16(input)?;
+    let (input, ether) = number::streaming::be_u16(input)?;
+
+    Ok((input, ether.into()))
+}
+pub fn parse_ethernet_frame(input: &[u8]) -> IResult<&[u8], EthernetFrame> {
+    let (input, direction) = number::streaming::be_u16(input)?;
+    let (input, ethtype) = number::streaming::be_u16(input)?;
+    let (input, addr_length) = number::streaming::be_u16(input)?;
+    let (input, source_mac) = mac_address(input)?;
+    let (input, ethertype) = parse_ethertype(input)?;
+
+    Ok((
+        input,
+        EthernetFrame {
+            direction,
+            ethtype,
+            addr_length,
+            source_mac,
+            ethertype,
+        },
+    ))
+}
--- anomaly_detector/src/main.rs
+++ anomaly_detector/src/main.rs
@@ -1,103 +1,112 @@
 use pcap::Capture;
-use nom::{number, IResult, Finish, bytes};
-use pktparse::{ethernet, ipv4, tcp};
-use pktparse::ethernet::{MacAddress, EtherType};
+use std::collections::HashMap;
+use easy_error::{bail, Error};
+use pktparse::{ipv4, tcp};
 use sawp::parser::{Direction, Parse};
-use sawp::error::Error;
 use sawp::error::ErrorKind;
-use sawp_modbus::{Modbus, Message};
+use sawp_modbus::{Modbus, Message, Data, Function, Flags, AccessType, CodeCategory, ErrorFlags};
+mod ethernet;
+use ethernet::{*};
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct EthernetFrame {
-    pub direction: u16,
-    pub ethtype: u16,
-    pub addr_length: u16,
-    pub source_mac: MacAddress,
-    pub ethertype: EtherType,
+
+/// Breakdown of the parsed modbus transaction
+#[derive(Debug, PartialEq)]
+pub struct Transaction {
+    pub transaction_id: u16,
+    pub valid: bool,
+    pub unit_id: u8,
+    pub function: Function,
+    pub access_type: Flags<AccessType>,
+    pub category: Flags<CodeCategory>,
+    pub request_data: Data,
+    pub response_data: Data,
+    pub error_flags: Flags<ErrorFlags>,
 }
 
-pub(crate) fn mac_address(input: &[u8]) -> IResult<&[u8], MacAddress> {
-    let (input, mac) = bytes::streaming::take(6u8)(input)?;
 
-    Ok((input, MacAddress(<[u8; 6]>::try_from(mac).unwrap())))
-}
 
-fn parse_ethertype(input: &[u8]) -> IResult<&[u8], EtherType> {
-    let (input, _) = number::streaming::be_u16(input)?;
-    let (input, ether) = number::streaming::be_u16(input)?;
 
-    Ok((input, ether.into()))
-}
-pub fn parse_ethernet_frame(input: &[u8]) -> IResult<&[u8], EthernetFrame> {
-    let (input, direction) = number::streaming::be_u16(input)?;
-    let (input, ethtype) = number::streaming::be_u16(input)?;
-    let (input, addr_length) = number::streaming::be_u16(input)?;
-    let (input, source_mac) = mac_address(input)?;
-    let (input, ethertype) = parse_ethertype(input)?;
-
-    Ok((
-        input,
-        EthernetFrame {
-            direction,
-            ethtype,
-            addr_length,
-            source_mac,
-            ethertype,
-        },
-    ))
-}
-
-fn parse_modbus(input: &[u8], direction: Direction) -> std::result::Result<&[u8], Error> {
+fn parse_modbus(input: &[u8], direction: Direction) -> std::result::Result<Option<Message>, Error> {
     let modbus = Modbus::default();
-    let mut bytes = input;
-    while bytes.len() > 0 {
-        // If we know that this is a request or response, change the Direction
-        // for a more accurate parsing
-        match modbus.parse(bytes, direction.clone()) {
-            // The parser succeeded and returned the remaining bytes and the parsed modbus message
-            Ok((rest, Some(message))) => {
-                println!("Modbus message: {:?}", message);
-                bytes = rest;
-            }
-            // The parser recognized that this might be modbus and made some progress,
-            // but more bytes are needed
-            Ok((rest, None)) => return Ok(rest),
-            // The parser was unable to determine whether this was modbus or not and more
-            // bytes are needed
-            Err(Error { kind: ErrorKind::Incomplete(_) }) => return Ok(bytes),
-            // The parser determined that this was not modbus
-            Err(e) => return Err(e)
+    let  bytes = input;
+
+    // If we know that this is a request or response, change the Direction
+    // for a more accurate parsing
+    match modbus.parse(bytes, direction.clone()) {
+        // The parser succeeded and returned the remaining bytes and the parsed modbus message
+        Ok((_rest, Some(message))) => {
+            return Ok(Some(message));
         }
+        // The parser recognized that this might be modbus and made some progress,
+        // but more bytes are needed
+        Ok((_rest, None)) => bail!("not parsed"),
+        // The parser was unable to determine whether this was modbus or not and more
+        // bytes are needed
+        Err(sawp::error::Error { kind: ErrorKind::Incomplete(_) }) => return Ok(None),
+        // The parser determined that this was not modbus
+        Err(_) => bail!("other error")
     }
-    Ok(bytes)
+    
+
 }
 fn main() -> Result<(), nom::error::ErrorKind> {
 
 let mut cap = Capture::from_file("../tcpdump/tcpdump.pcap").unwrap();
 
+    let mut sessions: HashMap<u16, Message> = HashMap::new();
     while let Ok(packet) = cap.next_packet() {
-
+            let time = packet.header.ts;
             if let Ok((remaining, eth_frame)) = parse_ethernet_frame(packet.data) {
 
                 if eth_frame.ethertype != ethernet::EtherType::IPv4 {
-                    println!("{:?}", eth_frame);
+                   // println!("{:?}", eth_frame);
                     continue;
                 }
 
-                println!("{:?}", eth_frame);
+                //println!("{:?}", eth_frame);
 
-                if let Ok((remaining, ipv4_packet) )= ipv4::parse_ipv4_header(remaining) {
-                    println!("{:?}", ipv4_packet);
+                if let Ok((remaining, _ipv4_packet) )= ipv4::parse_ipv4_header(remaining) {
+                    //println!("{:?}", ipv4_packet);
                     if let Ok((remaining, tcp_packet) )= tcp::parse_tcp_header(remaining) {
-                        println!("{:?}", tcp_packet);
+                        //println!("{:?}", tcp_packet);
                         let direction: Direction;
                         match tcp_packet.source_port {
                             502 => direction = Direction::ToClient,
                             _ => direction = Direction::ToServer,
 
                         }
-                    _ =parse_modbus(remaining, direction);
+                    match parse_modbus(remaining, direction) {
+                        Ok(Some(mut message)) => {
+                            if !message.error_flags.is_empty() {
+                                panic!("{:?}", message);
+                            }
+                            match sessions.remove_entry(&message.transaction_id) {
+                                Some(request) => {
+                                    let valid:bool = message.matches(&request.1); 
+                                    let trans = Transaction{
+                                        transaction_id: message.transaction_id,
+                                        valid,
+                                        unit_id: message.unit_id,
+                                        function: request.1.function,
+                                        access_type: request.1.access_type,
+                                        category: request.1.category,
+                                        request_data: request.1.data,
+                                        response_data: message.data,
+                                        error_flags: message.error_flags,
+                                    };
+                                    println!("{:x?}", trans);
+                                }
+                                None => {sessions.insert(message.transaction_id, message);},
+                            }
+                        }
+                        Err(e) => {
+                            println!("Error parsing modbus {:?}", e);
+                        }
+                        Ok(None) => {
+                            println!("Not modbus");
+                        }
+                    }
+                    
                 }
             }
         }
